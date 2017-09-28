565615c57c5556bef14e164aca7aae63215e6073
A.J. Bale <abalejr@gmail.com>
2017-09-28 14:30:40 -0500

    Click for new word on win, Remove most console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
     //establish currentWord as a Global Variable
     var currentWord = "";
+    //establish the list of words as a  Global variable
+    var words = ["Corneria", "Meteo", "Sector Y", "Fichina", "Katina", "Aquas", "Sector X", "Solar", "Zoness", "Titania", "Macbeth", "Sector Z", "Bolse", "Area Six", "Venom"];
 
     var newCurrentWord = function() {
-        //establish the list of words as a variable within the newCurrentWord function
-        var words = ["Corneria", "Meteo", "Sector Y", "Fichina", "Katina", "Aquas", "Sector X", "Solar", "Zoness", "Titania", "Macbeth", "Sector Z", "Bolse", "Area Six", "Venom"];
         //Clear #current-word element to make room for new word
         $("#current-word").empty();
         //Pick a random word from the list
         var pickedWord = words[Math.floor(Math.random()*words.length)].toUpperCase();
         //Check if pickedWord matches currentWord, and run the next 2 lines of code over and over until that is not the case
         while (pickedWord == currentWord) {
-            //Test to make sure repeats are being caught
-            console.log("pickedWord == currentWord")
             //If pickedWord is the same as the currentWord, randomly pick a new one
             pickedWord = words[Math.floor(Math.random()*words.length)].toUpperCase();
         }
@@ -20,35 +18,50 @@ $(document).ready(function(){
         currentWord = pickedWord;
         //Start looping through each letter in currentWord
         for (var i = 0; i < currentWord.length; i++) {
-            //Add a <span> tag inside the #current-word element for each letter
-            $("#current-word").append("<span id=\"" + i + "\">" + currentWord[i] + "</span>");
+            //Add a <div> tag inside the #current-word element for each letter
+            $("#current-word").append("<div id=\"" + i + "\">" + currentWord[i] + "</div>");
+            //Define a local variable for the selector of the <div> containing the current letter
+            var currentLetterDiv = $("#" + i.toString());
+            //Check if that div DOES NOT contain a space
+            if (currentLetterDiv.text() != " ") {
+                //Remove the letter from the div if it is not a space
+                currentLetterDiv.empty();
+            } else {
+                currentLetterDiv.attr("style", "border-bottom: 0");
+            }
         }
-        //Make sure those letters are not visible on the page by default
-        $("#current-word").children().hide();
         //Output currentWord to console to test function
         console.log(currentWord);
     };
     //Run this when the page loads, so players can jump in right away
     newCurrentWord();
 
-    
-    //Listen for keyboard key to be released, establish behavior
+
+    //Listen for keyboard key to be released
     $(document).keyup(function(e) {
         //First, start looping through each letter of currentWord
         for (var i = 0; i < currentWord.length; i++) {
-            //Check to see if that character is present in currentWord
+            //Check to see if tthe pressed character is present in currentWord
             if (String.fromCharCode(e.which) == currentWord[i]) {
-                //Define a variable for the <span> containing a matched letter
-                var matchedLetterSpan = $("#" + i.toString());
+                //Define a local variable for the selector of the <div> containing a matched letter
+                var matchedLetterDiv = $("#" + i.toString());
                 //Show that character
-                matchedLetterSpan.show();
+                if (matchedLetterDiv.text() != " ") {
+                    matchedLetterDiv.text(String.fromCharCode(e.which));
+                }
             }
+        }
+        if ($("#current-word").text() == currentWord) {
+            $("#win-message").text();
+        }
+    });
+
+    $(document).click(function() {
+        while ($("#current-word").text() == currentWord) {
+            newCurrentWord();
         }
     });
 
     //TESTS - everything below here is added just to test functionality
     //Handler below makes it easy to test newCurrentWord function - just click anywhere on the page to run newCurrentWord
-    $(document).click(function() {
-        newCurrentWord();
-    });
 });
