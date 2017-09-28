5b6a8c57910185d4925d67f8df66829d8592618a
A.J. Bale <abalejr@gmail.com>
2017-09-28 15:57:54 -0500

    Add Click for New Word Message

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,33 +1,36 @@
 $(document).ready(function(){
-    //establish currentWord as a Global Variable
-    var currentWord = "";
     //establish the list of words as a  Global variable
-    var words = [{word:"Corneria", img:"", win:"", song:""},
-                {word:"Meteo", img:"", win:"", song:""},
-                {word:"Sector Y", img:"", win:"", song:""},
-                {word:"Fichina", img:"", win:"", song:""},
-                {word:"Katina", img:"", win:"", song:""},
-                {word:"Aquas", img:"", win:"", song:""},
-                {word:"Sector X", img:"", win:"", song:""},
-                {word:"Solar", img:"", win:"", song:""},
-                {word:"Zoness", img:"", win:"", song:""},
-                {word:"Titania", img:"", win:"", song:""},
-                {word:"Macbeth", img:"", win:"", song:""},
-                {word:"Sector Z", img:"", win:"", song:""},
-                {word:"Bolse", img:"", win:"", song:""},
-                {word:"Area Six", img:"", win:"", song:""},
-                {word:"Venom", img:"", win:"", song:""}];
+    var words = [{word:"Corneria", img:"", win:"TEST", song:""},
+                {word:"Meteo", img:"", win:"TEST", song:""},
+                {word:"Sector Y", img:"", win:"TEST", song:""},
+                {word:"Fichina", img:"", win:"TEST", song:""},
+                {word:"Katina", img:"", win:"TEST", song:""},
+                {word:"Aquas", img:"", win:"TEST", song:""},
+                {word:"Sector X", img:"", win:"TEST", song:""},
+                {word:"Solar", img:"", win:"TEST", song:""},
+                {word:"Zoness", img:"", win:"TEST", song:""},
+                {word:"Titania", img:"", win:"TEST", song:""},
+                {word:"Macbeth", img:"", win:"TEST", song:""},
+                {word:"Sector Z", img:"", win:"TEST", song:""},
+                {word:"Bolse", img:"", win:"TEST", song:""},
+                {word:"Area Six", img:"", win:"TEST", song:""},
+                {word:"Venom", img:"", win:"TEST", song:""}];
+    //establish currentWord and its div as Global Variables
+    var currentWord = "";
     var currentWordDiv = $("#current-word");
+    var currentWordIndex = Math.floor(Math.random()*words.length);
+    var winMessageDiv = $("#win-message");
 
     var newCurrentWord = function() {
         //Clear #current-word element to make room for new word
         currentWordDiv.empty();
-        //Pick a random word from the list
-        var pickedWord = words[Math.floor(Math.random()*words.length)].word.toUpperCase();
         //Check if pickedWord matches currentWord, and run the next 2 lines of code over and over until that is not the case
+        //Pick a random word from the list
+        var pickedWord = words[currentWordIndex].word.toUpperCase();
         while (pickedWord == currentWord) {
             //If pickedWord is the same as the currentWord, randomly pick a new one
-            pickedWord = words[Math.floor(Math.random()*words.length)].word.toUpperCase();
+            currentWordIndex = Math.floor(Math.random()*words.length);
+            pickedWord = words[currentWordIndex].word.toUpperCase();
         }
         //Once pickedWord is different than currentWord, make it the new currentWord
         currentWord = pickedWord;
@@ -54,25 +57,27 @@ $(document).ready(function(){
 
     //Listen for keyboard key to be released
     $(document).keyup(function(e) {
+        var pressedCharacter = String.fromCharCode(e.which);
         //First, start looping through each letter of currentWord
         for (var i = 0; i < currentWord.length; i++) {
             //Check to see if tthe pressed character is present in currentWord
-            if (String.fromCharCode(e.which) == currentWord[i]) {
+            if (pressedCharacter == currentWord[i]) {
                 //Define a local variable for the selector of the <div> containing a matched letter
                 var matchedLetterDiv = $("#" + i.toString());
                 //Show that character
                 if (matchedLetterDiv.text() != " ") {
-                    matchedLetterDiv.text(String.fromCharCode(e.which));
+                    matchedLetterDiv.text(pressedCharacter);
                 }
             }
         }
         if (currentWordDiv.text() == currentWord) {
-            $("#win-message").text();
+            winMessageDiv.html(words[currentWordIndex].win + "<br><div id=\"win-sub\">Click Anywhere for a New Word.</div>");
         }
     });
 
     $(document).click(function() {
         if (currentWordDiv.text() == currentWord) {
+            winMessageDiv.text(words[currentWordIndex].win);
             newCurrentWord();
         }
     });
