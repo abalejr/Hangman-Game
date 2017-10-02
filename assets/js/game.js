2cf0e31705f97174224bc6eb3a0852c477221dd2
A.J. Bale <abalejr@gmail.com>
2017-10-02 00:52:37 -0500

    Stop registering guesses after win or lose, Add images and paths for them in word Objects

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     //establish the list of words as a  Global variable
-    var words = [{word:"Corneria", img:"", win:"Former Army Base", song:""},
-                {word:"Meteo", img:"", win:"Ateroid Belt", song:""},
-                {word:"Sector Y", img:"", win:"Enter Star Wolf", song:""},
-                {word:"Fichina", img:"", win:"Former Defense Base", song:""},
-                {word:"Katina", img:"", win:"Frontline Base", song:""},
-                {word:"Aquas", img:"", win:"Terror of the Deep", song:""},
-                {word:"Sector X", img:"", win:"Space Base", song:""},
-                {word:"Solar", img:"", win:"The Sun", song:""},
-                {word:"Zoness", img:"", win:"Toxic Waste Area", song:""},
-                {word:"Titania", img:"", win:"Arid Desert", song:""},
-                {word:"Macbeth", img:"", win:"Venom Army Supply Base", song:""},
-                {word:"Sector Z", img:"", win:"Great Fox Ambush", song:""},
-                {word:"Bolse", img:"", win:"The Last Hurrah", song:""},
-                {word:"Area Six", img:"", win:"Defense Station", song:""},
-                {word:"Venom", img:"", win:"Andross' Homeworld", song:""}];
+    var words = [{word:"Corneria", img:"assets/img/Corneria.jpg", win:"Former Army Base", song:""},
+                {word:"Meteo", img:"assets/img/Meteo.jpg", win:"Ateroid Belt", song:""},
+                {word:"Sector Y", img:"assets/img/Sectory.jpg", win:"Enter Star Wolf", song:""},
+                {word:"Fichina", img:"assets/img/Fichina.jpg", win:"Former Defense Base", song:""},
+                {word:"Katina", img:"assets/img/Katina.jpg", win:"Frontline Base", song:""},
+                {word:"Aquas", img:"assets/img/Aquas.jpg", win:"Terror of the Deep", song:""},
+                {word:"Sector X", img:"assets/img/Sectorx.jpg", win:"Space Base", song:""},
+                {word:"Solar", img:"assets/img/Solar.jpg", win:"The Sun", song:""},
+                {word:"Zoness", img:"assets/img/Zoness.jpg", win:"Toxic Waste Area", song:""},
+                {word:"Titania", img:"assets/img/Titania.jpg", win:"Arid Desert", song:""},
+                {word:"Macbeth", img:"assets/img/Macbeth.jpg", win:"Venom Army Supply Base", song:""},
+                {word:"Sector Z", img:"assets/img/Sectorz.jpg", win:"Great Fox Ambush", song:""},
+                {word:"Bolse", img:"assets/img/Bolse.jpg", win:"The Last Hurrah", song:""},
+                {word:"Area Six", img:"assets/img/Area6.jpg", win:"Defense Station", song:""},
+                {word:"Venom", img:"assets/img/Venom.jpg", win:"Andross' Homeworld", song:""}];
     //establish currentWord and its div as Global Variables
     var currentWord = "";
     var currentWordDiv = $("#current-word");
@@ -24,10 +24,10 @@ $(document).ready(function(){
     var guessLeftDiv = $("#guess-left");
     var winsSpan = $("#wins");
     var wins = 0;
-    var guessLeft = 0;
+    var guessLeft = 6;
     console.log(guessLeft);
 
-    var newCurrentWord = function() {
+    var NewCurrentWord = function() {
         //Clear #current-word to make room for new word
         currentWordDiv.empty();
         guessedDiv.empty();
@@ -43,6 +43,7 @@ $(document).ready(function(){
         }
         //Once pickedWord is different than currentWord, make it the new currentWord
         currentWord = pickedWord;
+        $("#display").html("<img src=\"" + words[currentWordIndex].img + "\" />");
         //Start looping through each letter in currentWord
         for (var i = 0; i < currentWord.length; i++) {
             //Add a <div> tag inside the #current-word element for each letter
@@ -62,58 +63,68 @@ $(document).ready(function(){
         console.log(currentWord);
     };
     //Run this when the page loads, so players can jump in right away
-    newCurrentWord();
+    NewCurrentWord();
 
 
     //Listen for keyboard key to be released
-    $(document).keyup(function(e) {
-        //Check if the key pressed is a letter
-        if (47 < e.which && e.which < 91) {
-            //if so, convert that letter to a string
-            var pressedLetter = String.fromCharCode(e.which);
-            //First, start looping through each letter of currentWord
-            if (currentWord.indexOf(pressedLetter) > -1) {
-                for (var i = 0; i < currentWord.length; i++) {
-                    //Check to see if the pressed character is present in currentWord
-                    if (pressedLetter == currentWord[i]) {
-                        //Define a local variable for the selector of the <div> containing a matched letter
-                        var matchedLetterDiv = $("#" + i.toString());
-                        //Unless that charachter is a space, show that character
-                        if (matchedLetterDiv.text() != " ") {
-                            matchedLetterDiv.text(pressedLetter);
+    var Guess = function(e) {
+        if (currentWordDiv.text() != currentWord && guessLeft != 0) {
+            //Check if the key pressed is a letter
+            if (47 < e.which && e.which < 91) {
+                //if so, convert that letter to a string
+                var pressedLetter = String.fromCharCode(e.which);
+                //First, start looping through each letter of currentWord
+                if (currentWord.indexOf(pressedLetter) > -1) {
+                    for (var i = 0; i < currentWord.length; i++) {
+                        //Check to see if the pressed character is present in currentWord
+                        if (pressedLetter == currentWord[i]) {
+                            //Define a local variable for the selector of the <div> containing a matched letter
+                            var matchedLetterDiv = $("#" + i.toString());
+                            //Unless that charachter is a space, show that character
+                            if (matchedLetterDiv.text() != " ") {
+                                matchedLetterDiv.text(pressedLetter);
+                            }
                         }
                     }
+                //If there is no match...
+                } else { 
+                    //Check if the letter has already been pressed
+                    if (guessedDiv.text().indexOf(pressedLetter) == -1) {
+                        //If not, reduce the number of guesses left, and
+                        guessLeft -= 1;
+                        //update that on the page
+                        guessLeftDiv.text(guessLeft);
+                        //then, add a new div with that letter in it
+                        guessedDiv.append("<div>" + pressedLetter + "</div");
+                    }
+                    //Check if there are no guesses left
+                    if (guessLeft == 0) {
+                        //if so, 
+                        winMessageDiv.html("Mission Failed <br><div id=\"win-sub\">Click Anywhere for a New Word.</div>");
+                    }
                 }
-            //If there is no match...
-            } else {
-                //Check if the letter has already been pressed
-                if (guessedDiv.text().indexOf(pressedLetter) == -1) {
-                    //If not, reduce the number of guesses left, and
-                    guessLeft -= 1;
-                    //update that on the page
-                    guessLeftDiv.text(guessLeft);
-                    //then, add a new div with that letter in it
-                    guessedDiv.append("<div>" + pressedLetter + "</div");
+                //Check if the word has been completed
+                if (currentWordDiv.text() == currentWord) {
+                    //If so, increase the number of wins by 1 and update that on the page
+                    wins += 1;
+                    winsSpan.text(wins);
+                    //replace #win-message with the win property of the current word and the Click for New Word message
+                    winMessageDiv.html(words[currentWordIndex].win + "<br><div id=\"win-sub\">Click Anywhere for a New Word.</div>");
                 }
-                if (guessLeft == 0) {
-                    winMessageDiv.html("Mission Failed<br><div id=\"win-sub\">Click Anywhere for a New Word.</div>");
-                }
-            }
-            //Check if the word has been completed
-            if (currentWordDiv.text() == currentWord) {
-                //If so, increase the number of wins by 1 and update that on the page
-                wins += 1;
-                winsSpan.text(wins);
-                //replace #win-message with the win property of the current word and the Click for New Word message
-                winMessageDiv.html(words[currentWordIndex].win + "<br><div id=\"win-sub\">Click Anywhere for a New Word.</div>");
             }
         }
-    });
+    };
 
+    $(document).keyup(Guess);
+
+    //Whenever someone clicks...
     $(document).click(function() {
-        if (currentWordDiv.text() == currentWord) {
+        //Check if the player has won or lost
+        if (currentWordDiv.text() == currentWord || guessLeft == 0) {
+            //If so, remove the Click for New Word text and...
             winMessageDiv.text(words[currentWordIndex].win);
-            newCurrentWord();
+            //Pick a new word
+            NewCurrentWord();
         }
     });
 
